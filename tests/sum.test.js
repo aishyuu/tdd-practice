87a00504e9ae58810a8e5c9df7f59c5275edcfd5
aishyuu <ivanmatias1331@gmail.com>
2024-03-14 18:08:18 -0700

    Made the first few tests

---
+++ tests/sum.test.js
@@ -0,0 +1,85 @@
+const sum = require("../code/sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("Adds 2 and 2 to equal 4", () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+test("Object Assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("Adding 2 and 2 does not equal 3", () => {
+  expect(sum(2, 2)).not.toBe(3);
+});
+
+test("null", () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeDefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+})
+
+test("zero", () => {
+    const z = 0
+    expect(z).toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeDefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+})
+
+test("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    expect(value).toEqual(4);
+})
+
+test("adding floating point numbers", () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+})
+
+test("There is no I in team", () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test("but there is a 'stop' in Christopher", () => {
+    expect('Christopher').toMatch(/stop/);
+})
+
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+];
+
+test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+})
+
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK!');
+}
+
+test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK!');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+})
